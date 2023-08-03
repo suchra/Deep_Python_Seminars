a346478ddcc07a718c8a6af49172cc02d165b30c
[Zukhra <zuhruschka@mail.ru>
2023-08-03 16:50:34 +0400

    "Seminar4"

---
+++ functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# транспонирование матрицы
+def transposing(matrix):
+    """Функция для транспонирования квадратной матрицы, принимает на вход коллекцию"""
+    return np.transpose(matrix)
+
+
+def reverse_kwargs(**kwargs):
+    """Функция принимающая на вход только ключевые параметры(kwargs) и возвращающая словарь,
+       где ключ — значение переданного аргумента, а значение — имя аргумента."""
+
+    try:
+        new_dict = {
+            value: key for key, value in kwargs.items()
+        }
+        return new_dict
+    except TypeError:
+        new_dict = {
+            str(value): key for key, value in kwargs.items()
+        }
+        return new_dict
